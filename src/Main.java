b33e6bafb71e88e51f1770e412c46e90cabd1f08
LiorBiran <liorbirangd@gmail.com>
2023-09-12 11:26:15 +0200

    /

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,26 @@ public class Main {
     private static Scanner scanner;
     private static String playerName;
     private static int playerBirthYear;
-    private static ArrayList<AdvantureNode> nodes;
+    // Going to hold the text printed for the first "chapter"
+    private static ArrayList<String> advantureChapters;
 
     public static void main(String[] args) throws Exception {
         scanner = new Scanner(System.in);
         printTitle();
         readInitInput();
-        initNodes();
+        initAdvantureChapters();
         startAdvanture();
     }
-    
+
+    // Run the advanture, only first node for now
     private static void startAdvanture() {
-        AdvantureNode current=nodes.get(0);
         String input;
-        //Run through all nodes until reacing a null node
-        //Print node text, and wait for player's input
-        while(current!=null){
-            System.out.print(current.text);
+        // Run through all the chapters (1 for now). Print text, and wait for player's input
+        for (int i = 0; i < advantureChapters.size(); i++) {
+            System.out.print(advantureChapters);
             scanner.nextLine();
-            input=scanner.nextLine();
+            input = scanner.nextLine();
             System.out.println(input);
-            AdvantureNode n = current.options.get(0);
-            current=n;
         }
     }
 
@@ -42,7 +40,8 @@ public class Main {
                 "\n" +
                 "by: Lior Biran");
     }
-    //Read name and birth year from the player
+
+    // Read name and birth year from the player
     private static void readInitInput() {
         System.out.print("What is your name?:  ");
         playerName = scanner.nextLine();
@@ -51,25 +50,26 @@ public class Main {
         playerBirthYear = scanner.nextInt();
         System.out.println("You are " + calculateAge(playerBirthYear) + " years old");
     }
-    //calculate and return player's age acording to a birth year
+
+    // calculate and return player's age acording to a birth year
     private static int calculateAge(int birthYear) {
         LocalDateTime now = LocalDateTime.now();
         int age = now.getYear() - playerBirthYear;
         return age;
     }
-    //Create the nodes with text
-    private static void initNodes(){
-        nodes=new ArrayList<AdvantureNode>();
-        AdvantureNode node=new AdvantureNode("\nYou are standing in an abandoned university office. There are neither students\n"+
-        "nor teachers around you. There's a table in front of you with various papers,\n"+
-        "pens, a small puzzle toy, and a calculator.\n"+
-        "A large window shows an empty office building; there are no Zombies in the empty\n"+
-        "building (as far as you can tell). Behind you is a dark and mysterious door that\n"+
-        "leads to a well-lit corridor with a fireproof ceiling and floor. You feel a\n"+
-        "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be\n"+
-        "heard in the distance. You are not thirsty, but you rather have a craving for\n"+
-        "justice.\n"+
-        "What would you like to do?: ",null);
-        nodes.add(node);
+
+    // Saves the first chapter text to a variable
+    private static void initAdvantureChapters() {
+        advantureChapters=new ArrayList<>();
+        advantureChapters. add( "\nYou are standing in an abandoned university office. There are neither students\n" +
+                "nor teachers around you. There's a table in front of you with various papers,\n" +
+                "pens, a small puzzle toy, and a calculator.\n" +
+                "A large window shows an empty office building; there are no Zombies in the empty\n" +
+                "building (as far as you can tell). Behind you is a dark and mysterious door that\n" +
+                "leads to a well-lit corridor with a fireproof ceiling and floor. You feel a\n" +
+                "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be\n" +
+                "heard in the distance. You are not thirsty, but you rather have a craving for\n" +
+                "justice.\n" +
+                "What would you like to do?: ");
     }
 }
