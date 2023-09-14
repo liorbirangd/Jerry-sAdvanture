b4dbc3c90029cd57aef6f864d72cacc61f50f8d8
LiorBiran <liorbirangd@gmail.com>
2023-09-14 10:58:33 +0200

    .

---
+++ src/Enums/InputCode.java
@@ -0,0 +1,12 @@
+package Enums;
+public enum inputCode{
+    Invalid,
+    OpenDoor,
+    GoNorth,
+    GoEast,
+    GoSouth,
+    GoWest,
+    TakeItem,
+    DropItem,
+    UseItem
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import Enums.inputCode;
+
 import java.time.LocalDateTime;
 
 public class Main {
@@ -15,22 +18,60 @@ public class Main {
         initNodes();
         startAdvanture();
     }
-    
+
     private static void startAdvanture() {
-        AdvantureNode current=nodes.get(0);
+        AdvantureNode current = nodes.get(0);
         String input;
-        //Run through all nodes until reacing a null node
-        //Print node text, and wait for player's input
-        while(current!=null){
+        // Run through all nodes until reacing a null node
+        // Print node text, and wait for player's input
+        while (current != null) {
             System.out.print(current.text);
-            scanner.nextLine();
-            input=scanner.nextLine();
+
             System.out.println(input);
             AdvantureNode n = current.options.get(0);
-            current=n;
+            current = n;
         }
     }
 
+    private static inputCode waitForInput() {
+        String input = scanner.nextLine();
+        inputCode code = inputCode.Invalid;
+        switch (input.toLowerCase()) {
+            case "open the door":
+            case "open door":
+                code = inputCode.OpenDoor;
+                break;
+            case "go north":
+            case "north":
+                code = inputCode.GoNorth;
+                break;
+            case "go east":
+            case "east":
+                code = inputCode.GoEast;
+                break;
+            case "go west":
+            case "west":
+                code = inputCode.GoWest;
+                break;
+            case "go south":
+            case "south":
+                code = inputCode.GoSouth;
+                break;
+            case "take item":
+                code = inputCode.TakeItem;
+                break;
+            case "drop item":
+                code = inputCode.DropItem;
+                break;
+            case "use item":
+            case "use":
+                code = inputCode.UseItem;
+                break;
+                
+        }
+    }
+
+    // #region GmaeStart
     // Print the ASCII art fot the game title
     private static void printTitle() {
         System.out.println("\r\n" +
@@ -42,7 +83,8 @@ public class Main {
                 "\n" +
                 "by: Lior Biran");
     }
-    //Read name and birth year from the player
+
+    // Read name and birth year from the player
     private static void readInitInput() {
         System.out.print("What is your name?:  ");
         playerName = scanner.nextLine();
@@ -50,26 +92,33 @@ public class Main {
         System.out.print("On what year were you born?:  ");
         playerBirthYear = scanner.nextInt();
         System.out.println("You are " + calculateAge(playerBirthYear) + " years old");
+        scanner.nextLine();
     }
-    //calculate and return player's age acording to a birth year
+
+    // calculate and return player's age acording to a birth year
     private static int calculateAge(int birthYear) {
         LocalDateTime now = LocalDateTime.now();
         int age = now.getYear() - playerBirthYear;
         return age;
     }
-    //Create the nodes with text
-    private static void initNodes(){
-        nodes=new ArrayList<AdvantureNode>();
-        AdvantureNode node=new AdvantureNode("\nYou are standing in an abandoned university office. There are neither students\n"+
-        "nor teachers around you. There's a table in front of you with various papers,\n"+
-        "pens, a small puzzle toy, and a calculator.\n"+
-        "A large window shows an empty office building; there are no Zombies in the empty\n"+
-        "building (as far as you can tell). Behind you is a dark and mysterious door that\n"+
-        "leads to a well-lit corridor with a fireproof ceiling and floor. You feel a\n"+
-        "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be\n"+
-        "heard in the distance. You are not thirsty, but you rather have a craving for\n"+
-        "justice.\n"+
-        "What would you like to do?: ",null);
+
+    // Create the nodes with text
+    private static void initNodes() {
+        nodes = new ArrayList<AdvantureNode>();
+        AdvantureNode node = new AdvantureNode(
+                "\nYou are standing in an abandoned university office. There are neither students\n" +
+                        "nor teachers around you. There's a table in front of you with various papers,\n" +
+                        "pens, a small puzzle toy, and a calculator.\n" +
+                        "A large window shows an empty office building; there are no Zombies in the empty\n" +
+                        "building (as far as you can tell). Behind you is a dark and mysterious door that\n" +
+                        "leads to a well-lit corridor with a fireproof ceiling and floor. You feel a\n" +
+                        "sense of Wi-Fi around you, the grinding of an LCD operated coffee machine can be\n" +
+                        "heard in the distance. You are not thirsty, but you rather have a craving for\n" +
+                        "justice.\n" +
+                        "What would you like to do?: ",
+                null);
         nodes.add(node);
     }
+    // #endregion
+
 }
